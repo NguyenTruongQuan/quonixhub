9c21639e4b3d35739e55146a1d3e398d799aa11a
Quonix <truongquan522@gmail.com>
2025-07-10 17:48:37 +0700

    Update init.lua

--- main/init.lua
+++ main/init.lua
@@ -2,16 +2,17 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
 -- Load Lib GUI
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/YourUser/QuonixHub/main/lib/OrionLib.lua"))()
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenTruongQuan/QuonixHub/main/lib/OrionLib.lua"))()
 local Window = OrionLib:MakeWindow({Name = "🤡 Quonix Hub - Blox Fruits", Free của Nguyễn Trường Quân = false, SaveConfig = true, ConfigFolder = "QuonixHub"})
 
 -- Load từng module
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YourUser/QuonixHub/main/modules/AutoFarm.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YourUser/QuonixHub/main/modules/AutoQuest.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YourUser/QuonixHub/main/modules/AutoStat.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YourUser/QuonixHub/main/modules/AutoSkill.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YourUser/QuonixHub/main/modules/ESP.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YourUser/QuonixHub/main/modules/Teleport.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenTruongQuan/QuonixHub/main/modules/AutoFarm.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenTruongQuan/QuonixHub/main/modules/AutoQuest.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenTruongQuan/QuonixHub/main/modules/AutoStat.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenTruongQuan/QuonixHub/main/modules/AutoSkill.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenTruongQuan/QuonixHub/main/modules/ESP.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenTruongQuan/QuonixHub/main/modules/Teleport.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenTruongQuan/QuonixHub/main/modules/AntiBan.lua"))()
 
 OrionLib:Init()
 -- Ẩn / hiện GUI bằng phím RightControl
